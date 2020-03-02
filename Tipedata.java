d96a48c667c8888e59d5830a967cd0d327e91d4e
Laksmana Iska <48617239+Chowlee19@users.noreply.github.com>
2020-03-02 20:19:58 +0700

    Add files via upload

---
+++ Tipedata.java
@@ -0,0 +1,20 @@
+public class Tipedata{
+	public static void main(String[] args){
+		byte a=100; byte b=50;
+		short s=10000;
+		int c=1000000;
+		long d=1000000L;
+		float f=234.5f;
+		double d1=123.4;
+		boolean one=true;
+		char letterA='A';
+		String makul="PBO asiique";
+		System.out.println("a="+a+" b="+b+"\nMakul="+makul+"\nHuruf="+letterA);
+		System.out.println(d);
+		
+		String nama="ilham";
+		int umur= 19;
+		System.out.println("nama="+nama+"\numur="+umur);
+	}
+}
+
